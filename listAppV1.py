4c5839e39c5134962cc73b6b8d3a87d62c6eeb78
PeregrineShade <71675396+PeregrineShade@users.noreply.github.com>
2021-03-09 13:26:33 -0700

    Add files via upload

---
+++ listAppV1.py
@@ -0,0 +1,19 @@
+def mainProgram():
+    myList = []
+    print("Hello, there! Lets work with lists.")
+    print("Choose from the following options. Type a number below!")
+    choice = input("1. Add to a list  , 2. Return the value at an index position!     ")
+    if choice == "1":
+        addToList()
+    elif choice == "2":
+        indexValues()
+    #TO ADD: 1. A way to loop the action ,  2. A way to quit ,  3. Think of repetition,
+def addToList():
+    print("Adding to a list.. That was.. a choice. Yikes!"
+    newItem = input("Type an Integer here.    ")
+    newList.append(int(newItem))
+
+def indexValue():
+
+if__name__ == "__main__":
+    mainProgram()
